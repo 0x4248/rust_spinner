acd9c33c7f32c9219c9f5bb9bac1405ebe392b94
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-07 09:48:26 +0100

    first commit

---
+++ src/spinner.rs
@@ -0,0 +1,28 @@
+/* Rust Spinner (spinner.rs)
+ * A simple spinner for Rust
+ */
+pub mod spinner {
+    use std::io::{self, Write};
+    use std::sync::{Arc, Mutex};
+    use std::thread;
+    use std::time::Duration;
+    
+    pub fn spinner_cleanup(){
+        print!("\r");
+        io::stdout().flush().unwrap();
+    }
+    
+    pub fn start_spinner(frames: Vec<String>, message: String, stop_spinner: Arc<Mutex<bool>>) -> thread::JoinHandle<()> {
+        thread::spawn(move || {
+            let mut frame_index = 0;
+
+            while !*stop_spinner.lock().unwrap() {
+                print!("\r{} {}", message, frames[frame_index]);
+                std::io::stdout().flush().unwrap();
+                frame_index = (frame_index + 1) % frames.len();
+                thread::sleep(Duration::from_millis(100));
+            }
+            io::stdout().flush().unwrap();
+        })
+    }
+}
