02aaad57a46784d2070c60163a5e98314b652307
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-04-11 11:58:17 +0100

    Username update
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- src/spinner.rs
+++ src/spinner.rs
@@ -1,8 +1,8 @@
 /* Rust Spinner (spinner.rs)
  * A simple spinner for Rust
- * Github: https://www.github.com/lewisevans2007/rust_spinner
+ * Github: https://www.github.com/0x4248/rust_spinner
  * Licence: GNU General Public License v3.0
- * By: Lewis Evans
+ * By: 0x4248
 */
 
 pub mod spinner {
